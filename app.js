ee84e4ff290e7e5627d083071e619463a87047c9
milan.zivanov <milan.zivanov@gmail.com>
2021-08-11 19:28:36 +0200

    Fetch a set of data from the GitHub API

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/data", (req, res) => {
-  res.end("Response from the API!");
+app.get("/data", async (req, res) => {
+
+  const query = `{ viewer { 
+    login } 
+  }`;
+  const url = "https://api.github.com/graphql";
+
+  const options = {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+      authorization: "bearer " + process.env.APIKEY,
+    },
+    body: JSON.stringify({ query: query }),
+  };
+
+  let response;
+  try {
+    response = await fetch(url, options);
+  } catch (error) {
+    console.error(error);
+  }
+  const data = await response.json();
+  console.log(data);
+
+  res.json(data);
+
 });
 
 
